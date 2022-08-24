b1e150f69e52ef8f99055ba4e629036e2673901d
niikoolaa04 <nikola04jovicic@gmail.com>
2022-08-24 22:38:46 +0200

    Changes

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,7 +3,7 @@ const { TrovoClient } = require("../dist/index");
 let trovoClient = new TrovoClient("", {
   checkLive: false,
   checkInterval: 10,
-  liveChannels: ["mudja"],
+  liveChannels: [],
 });
 
 trovoClient.channels.getChannelByName().then((res) => console.log(res))
