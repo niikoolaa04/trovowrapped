8a2faf8c5d2bc9cdb2fc768afe2c05f39affe11f
niikoolaa04 <nikola04jovicic@gmail.com>
2022-08-24 22:39:31 +0200

    Test

--- __tests__/index.js
+++
@@ -1,9 +0,0 @@
-const { TrovoClient } = require("../dist/index");
-
-let trovoClient = new TrovoClient("", {
-  checkLive: false,
-  checkInterval: 10,
-  liveChannels: [],
-});
-
-trovoClient.channels.getChannelByName().then((res) => console.log(res))
